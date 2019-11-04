a7e66f47aeaa57e3323f7b396adb2f390c54bbe1
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-04 16:57:50 +0800

    step07 fix todotasks model with no tests

--- db/migrate/20191104085421_create_todotasks.rb
+++ db/migrate/20191104085421_create_todotasks.rb
@@ -1,8 +1,8 @@
 class CreateTodotasks < ActiveRecord::Migration[5.2]
   def change
     create_table :todotasks do |t|
-      t.string :name
-      t.text :content
+      t.string "name"
+      t.text "content"
 
       t.timestamps
     end
--- test/models/todotask_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TodotaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
