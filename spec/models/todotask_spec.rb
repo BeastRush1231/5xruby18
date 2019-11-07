4ff7e82e035d68b10c6e2c6c293f4b02818217d7
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-07 17:53:05 +0800

    step12 model validates spec

--- spec/models/todotask_spec.rb
+++ spec/models/todotask_spec.rb
@@ -17,4 +17,11 @@ RSpec.describe Todotask, type: :model do
         expect(todotask.content).to eq "do task"
     end
   end
+
+  describe "建立 Todotask 任務&任務內容不能為空白" do 
+    it "Todotask 不空白任務內容" do 
+      todotask = Todotask.create(name: "",content: "")
+        expect(todotask.errors.full_messages).to eq ["Name 不能為空白", "Content 不能為空白"]
+    end
+  end
 end 
