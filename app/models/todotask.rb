9028b74530fe7b3f1978dedcc4eaf349dc7c7147
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-08 16:04:41 +0800

    step14 add start_time & end_time + asc by end_time

--- app/models/todotask.rb
+++ app/models/todotask.rb
@@ -2,4 +2,5 @@ class Todotask < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
   belongs_to :user
+  scope :order_by_end_time, -> { order(end_time: :asc) } 
 end
