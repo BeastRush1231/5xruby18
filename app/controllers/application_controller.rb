57bebde80b018f341cee97ff16dac26ee877a477
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-20 12:09:39 +0800

    step13 fix http_auth Figaro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  http_basic_authenticate_with name: ENV["http_auth_username"], password: ENV["http_auth_userpassword"] if Rails.env.production?
 
   def set_locale
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
