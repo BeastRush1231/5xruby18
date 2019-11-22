636a7cafae0d15978817902e47f4845ac11259b1
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-22 14:14:02 +0800

    step15 search feature spec

--- spec/features/todotasks_spec.rb
+++ spec/features/todotasks_spec.rb
@@ -40,7 +40,7 @@ RSpec.feature "Todotasks", type: :feature do
   end
 
   scenario "任務列表以結束時間(end_time)排序" do
-    FactoryBot.create(:todotask, name: "todotasks1", end_time: "2019-10-26 20:18:18 +0800")
+    FactoryBot.create(:todotask, name: "todotasks1")
     FactoryBot.create(:todotask, name: "todotasks2", end_time: "2019-10-26 19:18:18 +0800")
     visit root_path
     within 'tbody tr:nth-child(1)' do
@@ -49,5 +49,23 @@ RSpec.feature "Todotasks", type: :feature do
     within 'tbody tr:nth-child(2)' do
       expect(page).to have_content("todotasks1")
     end
+  end
+
+  let(:todotask1) { FactoryBot.create(:todotask, end_time: Time.now.yesterday) }
+  let(:todotask2) { FactoryBot.create(:todotask, end_time: Time.now) }
+
+  scenario "建立2個任務後 更改狀態" do
+    todotask1
+    todotask2
+
+    visit edit_todotask_url(todotask1.id)
+    select 'processing', from: 'todotask_status'
+    click_on "送出"
+    visit todotasks_path
+
+    expect(todotask1.reload.status).to eq "processing"
+    expect(todotask2.status).to eq "upcoming"
+    expect(find('table tbody tr:nth-child(1) td:nth-child(5)')).to have_content("processing")
+    expect(find('table tbody tr:nth-child(2) td:nth-child(5)')).to have_content("upcoming")
   end
 end
