6fb9f1ffaa1bbdb318d883f9f24306baaee7d4d4
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-11-29 17:12:12 +0800

    step17 fix gem data_cleaner

--- spec/features/todotasks_spec.rb
+++ spec/features/todotasks_spec.rb
@@ -1,18 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Todotasks", type: :feature do
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-
+ 
   before do
     FactoryBot.create(:user)
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -61,6 +60,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
